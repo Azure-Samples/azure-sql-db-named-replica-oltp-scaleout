12574f12ab8165a8c2ca5fce026afa11df787b1c
Davide Mauri <damauri@microsoft.com>
2021-03-28 17:12:09 -0700

    correctly wrap json result into a single column

--- app/Database/00-database.sql
+++ app/Database/00-database.sql
@@ -87,25 +87,27 @@ create or alter procedure api.get_shopping_cart
 @id bigint
 as
 set nocount on;
-select top (1)
-	c.cart_id,
-	c.[user_id],
-	json_query((
-		select
-			item_id as 'id',
-			quantity,
-			price,
-			json_query(item_details) as 'details'
-		from
-			dbo.shopping_cart as items
-		where
-			items.cart_id = c.cart_id
-		for json path
-	)) as items
-from 
-	dbo.shopping_cart c
-where
-	cart_id = @id
-for json auto, without_array_wrapper
+select ((
+	select top (1)
+		c.cart_id,
+		c.[user_id],
+		json_query((
+			select
+				item_id as 'id',
+				quantity,
+				price,
+				json_query(item_details) as 'details'
+			from
+				dbo.shopping_cart as items
+			where
+				items.cart_id = c.cart_id
+			for json path
+		)) as items
+	from 
+		dbo.shopping_cart c
+	where
+		cart_id = @id
+	for json auto, without_array_wrapper
+)) as json_result;
 go
 
